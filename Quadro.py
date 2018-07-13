d7ddb15e5269bc61d4925d2feb835af8f2897e7a
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2018-07-12 21:13:45 -0300

    Modificacoes em quadros

--- Quadro.py
+++ Quadro.py
@@ -1,28 +1,51 @@
 class QuadroDados:
-    def __init__(self,destination, source, payload):
-        self.delimiter = bytes.fromHex('7E')
-        self.length = bytes.fromHex(len(payload))
+    def __init__(self):
+        self.delimiter = None
+        self.length = None
+        self.sequence = None
+        self.destinationAddress = None
+        self.sourceAddress = None
+        self.payload = None
+        self.crc = None
+        self.sequenceField = False # Variável usada pra alternar entre 0 e 1
+
+    def novaMensagem(self, destination, source, payload):
+        self.delimiter = bytes.fromhex('7E')
+        self.length = bytes([len(payload)])
         self.sequence = self.calculoSequence()
-        self.destinationAddress = bytes.fromHex(destination)
-        self.sourceAddress = bytes.fromHex(source)
+        self.destinationAddress = str(destination).encode('ascii')
+        self.sourceAddress = str(source).encode('ascii')
         self.payload = str(payload).encode('ascii')
         self.crc = self.calculoCRC()
-
-        #80
-        #00
-    
+        return self
 
     #Todo: Calcular CRC
-    def calculoCRC(self);
-        return None
+    def calculoCRC(self):
+        #retorno inutil
+        return bytes.fromhex('7E')
     
-    #Todo: Calcular Sequence
+    # O quadro dados só é utilizado em envios, logo, 
+    # só vai varia o primeiro bit. Então ou é '80' ou '00'
     def calculoSequence(self):
-        
+        if self.sequenceField:
+            self.sequenceField = not self.sequenceField
+            return bytes.fromhex('80')
+        else:
+            self.sequenceField = not self.sequenceField
+            return bytes.fromhex('00')
     
     #A ideia aqui é retornar a mensagem de modo que ela esteja pronta para ser enviada
-    def __str__(self):
-        return str(str(self.delimiter)+str(self.length)+str(self.sequence)+str(self.destinationAddress)+str(self.sourceAddress)+str(self.payload())+str(self.crc)).encode('ascii')
+    #Depois de preparar a mensagem, limpamos a classe para ser utilziada novamente
+    def retornaMensagem(self):
+        mensagem = str(str(self.delimiter)+str(self.length)+str(self.sequence)+str(self.destinationAddress)+str(self.sourceAddress)+str(self.payload)+str(self.crc)).encode('ascii')
+        self.delimiter = None
+        self.length = None
+        self.sequence = None
+        self.destinationAddress = None
+        self.sourceAddress = None
+        self.payload = None
+        self.crc = None
+        return mensagem
 
 class QuadroConfirmacao:
     def __init__(self, destination, source):
@@ -30,3 +53,13 @@ class QuadroConfirmacao:
         self.sequence = None
         self.destinationAddress = destination
         self.sourceAddress = source
+
+#Exemplo de uso
+if __name__ == "__main__":
+    # A ideia é instânciar apenas uma variavel do quadro de dados 
+    # Em seguida, devemos criar novas mensagens usando a função "novaMensagem(destino, fonte, mensagem)" e pegar a mensagem com "retornaMensagem()"
+    quadroDados = QuadroDados()
+    mensagem1 = quadroDados.novaMensagem("192.168.0.1", "192.168.0.2", "ola mund").retornaMensagem()
+    mensagem2 = quadroDados.novaMensagem("192.168.0.3", "192.168.0.4", "huehue").retornaMensagem()
+    print(mensagem1)
+    print(mensagem2)
